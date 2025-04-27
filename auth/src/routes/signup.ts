1b20efcc1b432d6111e41a945f957f15043fe2c3
Ta-SeenJunaid <taseen.junaid@gmail.com>
2025-04-27 10:09:02 +0200

    password hashing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -19,12 +20,18 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
 
-    console.log('A user is created .............');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({ email });
+    if (existingUser) {
+      throw new BadRequestError('Email in use!!!!!!!!');
+    }
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
