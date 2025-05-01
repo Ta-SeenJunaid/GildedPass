7f061fd2e02dda33554f5f14672fbabc4b377408
Ta-SeenJunaid <taseen.junaid@gmail.com>
2025-05-01 15:54:04 +0200

    Signin and other fix

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { Password } from '../util/password';
+import { PasswordManager } from '../util/password-manager';
 
 interface UserAttrs {
   email: string;
@@ -15,20 +15,32 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
-    const hashedPassword = await Password.toHash(this.get('password'));
+    const hashedPassword = await PasswordManager.toHash(this.get('password'));
     this.set('password', hashedPassword);
   }
   done();
