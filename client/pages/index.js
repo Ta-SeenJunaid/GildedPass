1015b56d0442b19dd274b853a408008d64a36dc9
Ta-SeenJunaid <taseen.junaid@gmail.com>
2025-05-17 10:03:47 +0200

    server side rendering & signup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const Landing = () => {
-  return <h1>Landing !!!!</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>User is signed in!!!</h1>
+  ) : (
+    <h1>please sign in!!!</h1>
+  );
 };
 
-export default Landing;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
